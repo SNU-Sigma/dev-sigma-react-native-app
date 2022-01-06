916f9f0f907c4575baa0b4fa5ed7495742f40379
Jaewoo <tlsbamtol@snu.ac.kr>
2022-01-06 22:02:03 +0900

    only login ui

--- App.tsx
+++ App.tsx
@@ -1,22 +1,46 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import * as React from 'react'
+import { View, Text, TextInput, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-export default function App() {
+function HomeScreen() {
     const onPress = () => {}
     return (
-        <View>
-            <TextInput />
-            <TextInput />
-            <Button title='버튼' onPress={onPress} />
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+            <TextInput placeholder='email' />
+            <TextInput placeholder='password' />
+            <Button title='Log In' />
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
+function DetailsScreen() {
+    return (
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+            <Text>Details Screen</Text>
+        </View>
+    )
+}
+
+const Stack = createNativeStackNavigator()
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name='Home'
+                    component={HomeScreen}
+                    options={{ title: 'Overview' }}
+                />
+                <Stack.Screen name='Details' component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default App
