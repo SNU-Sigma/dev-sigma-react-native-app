929aef25df8aa47f6353a84c8550131f71e3846a
Jaewoo <tlsbamtol@snu.ac.kr>
2022-01-21 15:03:42 +0900

    bottom tab bar simplified

--- src/MainScreen/Main.tsx
+++ src/MainScreen/Main.tsx
@@ -1,12 +1,78 @@
-import { View, Text } from 'react-native'
+import { ColorPropType, Image } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import Post from './Post'
+import PrinterReservation from './PrinterReservation'
+import MyPage from './MyPage'
+
+const Tab = createBottomTabNavigator()
 
 export default function MainScreen() {
     return (
-        <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        <Tab.Navigator
+            initialRouteName='Post'
+            screenOptions={{ tabBarActiveTintColor: 'black' }}
         >
-            <Post />
-        </View>
+            <Tab.Screen
+                name='Post'
+                component={Post}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({
+                        color,
+                        size,
+                    }: {
+                        color: any
+                        size: any
+                    }) => (
+                        <MaterialCommunityIcons
+                            name='home'
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name='PrinterReservation'
+                component={PrinterReservation}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({
+                        color,
+                        size,
+                    }: {
+                        color: any
+                        size: any
+                    }) => (
+                        <MaterialCommunityIcons
+                            name='printer'
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name='MyPage'
+                component={MyPage}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({
+                        color,
+                        size,
+                    }: {
+                        color: any
+                        size: any
+                    }) => (
+                        <MaterialCommunityIcons
+                            name='face'
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
     )
 }
---
+++ src/MainScreen/PrinterReservation.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+export default function PrinterReseration() {
+    return (
+        <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+            <Text>Printer Reservation</Text>
+        </View>
+    )
+}
