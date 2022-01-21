7ec42af65401387b6a9cc9108e7250ea9c644c7b
gates02 <gates02@naver.com>
2022-01-21 20:48:31 +0900

    feat: 디자인 개선 및 Searchbar 위한 library 추가

--- src/MainScreen/Post.tsx
+++ src/MainScreen/Post.tsx
@@ -7,9 +7,11 @@ import {
     ContentText,
     MoreText,
 } from './PostStyles'
-import profile from '../assets/images/profile.png'
+import profile from '../assets/images/sigmaprofile.png'
 import picture from '../assets/images/picture.png'
 import adminmark from '../assets/images/adminmark.png'
+//import { SearchBar } from 'react-native-elements'
+// import { Searchbar } from 'react-native-paper'
 
 interface Post {
     id: number
@@ -31,7 +33,8 @@ const DATA: Array<Post> = [
     {
         id: 2,
         author: 'Jinseok',
-        content: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet',
+        content:
+            '안녕하세요, 관악 최강 로봇 동아리 SIGMA INTELLIGENCE입니다! 전기정보공학부 소속의 과 동아리이지만, 전기과 학생들 말고도 타 공대나 자연대, 더 나아가 미대 학생들도 같이 참여하고 있습니다. 덕분에 과 동아리의 장점인 과의 지원과',
         photos: ['photo url'],
         isAdmin: false,
     },
@@ -51,46 +54,63 @@ const DATA: Array<Post> = [
     },
 ]
 
+const renderItem = ({ item }: { item: Post }) => {
+    return (
+        <PostView>
+            <ProfileView>
+                <Image source={profile} style={{ width: 42, height: 42 }} />
+                <Text style={{ fontSize: 14, padding: 8 }}>{item.author}</Text>
+            </ProfileView>
+            {item.isAdmin && <AdminImage source={adminmark} />}
+            <ContentText numberOfLines={2}>{item.content}</ContentText>
+            <MoreText>더보기{'>'}</MoreText>
+            <View style={{ height: 131, flexDirection: 'row' }}>
+                <Image
+                    source={picture}
+                    style={{ width: 138, height: 131, marginHorizontal: 5 }}
+                />
+                <Image
+                    source={picture}
+                    style={{ width: 138, height: 131, marginHorizontal: 5 }}
+                />
+            </View>
+        </PostView>
+    )
+}
+
+const ListHeader = () => {
+    return <TitleText>SIGMA BOARD</TitleText>
+}
+
 export default function Post() {
-    const renderItem = ({ item }: { item: Post }) => {
-        return (
-            <PostView>
-                <ProfileView>
-                    <Image source={profile} style={{ width: 44, height: 44 }} />
-                    <Text style={{ fontSize: 14, padding: 8 }}>
-                        {item.author}
-                    </Text>
-                </ProfileView>
-                {item.isAdmin && <AdminImage source={adminmark} />}
-                <ContentText numberOfLines={2}>{item.content}</ContentText>
-                <MoreText>더보기{'>'}</MoreText>
-                <View style={{ height: 131, flexDirection: 'row' }}>
-                    <Image
-                        source={picture}
-                        style={{ width: 138, height: 131, marginHorizontal: 4 }}
-                    />
-                    <Image
-                        source={picture}
-                        style={{ width: 138, height: 131, marginHorizontal: 4 }}
-                    />
-                </View>
-            </PostView>
-        )
-    }
-
-    const ListHeader = () => {
-        return <TitleText>SIGMA BOARD</TitleText>
-    }
-
     return (
         <View
             style={{
-                flex: 1,
+                flex: 1, // 의미는 모르겠지만 이렇게 해야 잘 된다
                 justifyContent: 'center',
             }}
         >
+            <View
+                style={{
+                    borderWidth: 1,
+                    width: 334,
+                    height: 40,
+                    top: 45,
+                    position: 'absolute',
+                }}
+            />
+            <View
+                style={{
+                    borderWidth: 1,
+                    width: 334,
+                    height: 53,
+                    top: 95,
+                    position: 'absolute',
+                }}
+            />
             <FlatList
-                style={{ marginTop: 120 }}
+                showsHorizontalScrollIndicator={false}
+                style={{ marginTop: 170 }} // 검색창과 공지를 위한 공간
                 data={DATA}
                 ListHeaderComponent={ListHeader}
                 renderItem={renderItem}
--- src/MainScreen/PostStyles.ts
+++ src/MainScreen/PostStyles.ts
@@ -1,7 +1,8 @@
 import styled from '@emotion/native'
 
 export const TitleText = styled.Text({
-    paddingLeft: 12,
+    paddingLeft: 6,
+    paddingBottom: 7,
     fontSize: 14,
     fontWeight: 'bold',
 })
@@ -9,31 +10,36 @@ export const TitleText = styled.Text({
 export const PostView = styled.View({
     width: 335,
     height: 267,
-    marginBottom: 17,
+    marginBottom: 16,
     paddingHorizontal: 20,
     paddingBottom: 20,
-
-    backgroundColor: '#C4C4C4',
+    backgroundColor: '#FFFFFF',
     borderRadius: 23,
+    borderWidth: 1,
+    borderColor: '#C4C4C4',
+    shadowColor: 'rgba(0,0,0,0.25)',
+    shadowRadius: 9,
+    shadowOffset: { width: 3, height: 4 },
 })
 
 export const AdminImage = styled.Image({
     position: 'absolute',
-    top: 5,
-    left: 45,
+    top: 3,
+    left: 48,
     width: 21,
     height: 21,
 })
+
 export const ProfileView = styled.View({
-    height: 44,
-    marginTop: 9,
+    height: 42,
+    marginTop: 8,
     flexDirection: 'row',
     alignItems: 'center',
 })
 
 export const ContentText = styled.Text({
     height: 38,
-    marginTop: 6,
+    marginTop: 10,
     fontSize: 12,
 })
 
