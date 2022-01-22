8af2a1f41eb6a1dd0a7e69801b16f4cd4463b9d4
Jihoon Seo <jihoon416@snu.ac.kr>
2022-01-22 16:32:28 +0900

    feat(PrinterHorizontalSelectionView): 기본 UI 구현
    react-native-web으로는 안 보이는 문제 존재

--- src/PrinterReservation/view/PrinterDayHorizontalSelectionView.tsx
+++ src/PrinterReservation/view/PrinterDayHorizontalSelectionView.tsx
@@ -1,3 +1,58 @@
-export const PrinterDayHorizontalSelectionView = () => {
-    return <></>
+import { useMemo } from 'react'
+import { Dimensions, Text, View } from 'react-native'
+import Carousel from 'react-native-reanimated-carousel'
+import { CarouselRenderItem } from 'react-native-reanimated-carousel/lib/typescript/types'
+import { addDays, eachDayOfInterval, format } from 'date-fns'
+
+type Props = {
+    selectedDate: Date
+    onSelectedDateChange: (date: Date) => void
+}
+
+const renderItem: CarouselRenderItem<Date> = (itemInfo) => {
+    const date = itemInfo.item
+    return (
+        <View
+            style={{
+                flex: 1,
+                marginVertical: 16,
+                marginHorizontal: 16,
+                backgroundColor: 'white',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 8,
+            }}
+        >
+            <Text>{format(date, 'M/d')}</Text>
+            <Text>{format(date, 'eee')}</Text>
+        </View>
+    )
+}
+
+export const PrinterDayHorizontalSelectionView = (props: Props) => {
+    const { selectedDate, onSelectedDateChange } = props
+
+    const data: Array<Date> = useMemo(() => {
+        return eachDayOfInterval({
+            start: selectedDate,
+            end: addDays(selectedDate, 30),
+        })
+    }, [selectedDate])
+
+    return (
+        <View>
+            <Carousel
+                data={data}
+                renderItem={renderItem}
+                style={{
+                    width: Dimensions.get('window').width,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+                width={120}
+                loop={false}
+            />
+        </View>
+    )
 }
--- src/PrinterReservation/view/PrinterReservationScreen.tsx
+++ src/PrinterReservation/view/PrinterReservationScreen.tsx
@@ -1,14 +1,37 @@
+import { useState } from 'react'
 import styled from '@emotion/native'
 import { PrinterDayHorizontalSelectionView } from './PrinterDayHorizontalSelectionView'
 import { PrinterTimeVerticalSelectionView } from './PrinterTimeVerticalSelectionView'
+import { startOfDay } from 'date-fns'
+import { View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export const PrinterReservationScreen = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date())
+
+    const handleSelectedDateChange = (date: Date) => {
+        setSelectedDate(date)
+    }
+
+    const selectedDateSimplified = startOfDay(selectedDate)
+
     return (
         <Container>
-            <PrinterDayHorizontalSelectionView />
-            <PrinterTimeVerticalSelectionView />
+            <View style={{ flex: 1 }}>
+                <PrinterDayHorizontalSelectionView
+                    selectedDate={selectedDateSimplified}
+                    onSelectedDateChange={handleSelectedDateChange}
+                />
+            </View>
+            <View style={{ flex: 2 }}>
+                <PrinterTimeVerticalSelectionView />
+            </View>
         </Container>
     )
 }
 
-const Container = styled.View({})
+const Container = styled(SafeAreaView)({
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+})
