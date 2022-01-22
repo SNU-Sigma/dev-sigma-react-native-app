761ee67db96c8589637224d113a5f566d50afc96
gates02 <gates02@naver.com>
2022-01-22 14:55:10 +0900

    chore: bg color changed, uri source image, added 2x 3x images, elevation, etc.

--- src/MainScreen/Post.tsx
+++ src/MainScreen/Post.tsx
@@ -1,56 +1,69 @@
 import { View, Text, FlatList, Image } from 'react-native'
 import {
-    TitleText,
     PostView,
+    ProfileImage,
     AdminImage,
     ProfileView,
     ContentText,
     MoreText,
+    ImageView,
+    TitleText,
 } from './PostStyles'
-import profile from '../assets/images/sigmaprofile.png'
 import picture from '../assets/images/picture.png'
-import adminmark from '../assets/images/adminmark.png'
+import adminMark from '../assets/images/admin_mark.png'
 //import { SearchBar } from 'react-native-elements'
 // import { Searchbar } from 'react-native-paper'
 
-interface Post {
-    id: number
-    author: string
-    content: string
+type Post = {
+    id: string
+    authorName: string
+    profilePicture: string
+    date: string
     photos: Array<string>
+    content: string
     isAdmin: boolean
 }
 
 const DATA: Array<Post> = [
     {
-        id: 1,
-        author: 'Jinseok',
+        id: '1',
+        authorName: 'Jinseok',
+        profilePicture: '',
+        date: '',
+        photos: ['photo url'],
         content:
             '전기정보공학부 소속의 과 동아리이지만, 전기과 학생들 말고도 타 공대나 자연대, 더 나아가 미대 학생들도 같이 참여하고 있습니다. 동아리의 앱 개발이 한창 진행중입니다.',
-        photos: ['photo url'],
         isAdmin: true,
     },
     {
-        id: 2,
-        author: 'Jinseok',
+        id: '2',
+        authorName: 'Jinseok',
+        profilePicture: '',
+        date: '',
+        photos: ['photo url'],
         content:
-            '안녕하세요, 관악 최강 로봇 동아리 SIGMA INTELLIGENCE입니다! 전기정보공학부 소속의 과 동아리이지만, 전기과 학생들 말고도 타 공대나 자연대, 더 나아가 미대 학생들도 같이 참여하고 있습니다. 덕분에 과 동아리의 장점인 과의 지원과',
-        photos: ['photo url'],
+            '전기정보공학부 소속의 과 동아리이지만, 전기과 학생들 말고도 타 공대나 자연대, 더 나아가 미대 학생들도 같이 참여하고 있습니다. 동아리의 앱 개발이 한창 진행중입니다.',
         isAdmin: false,
     },
     {
-        id: 3,
-        author: 'Jinseok',
-        content: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet',
+        id: '3',
+        authorName: 'Jinseok',
+        profilePicture: '',
+        date: '',
         photos: ['photo url'],
-        isAdmin: false,
+        content:
+            '전기정보공학부 소속의 과 동아리이지만, 전기과 학생들 말고도 타 공대나 자연대, 더 나아가 미대 학생들도 같이 참여하고 있습니다. 동아리의 앱 개발이 한창 진행중입니다.',
+        isAdmin: true,
     },
     {
-        id: 4,
-        author: 'Jinseok',
-        content: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet',
+        id: '4',
+        authorName: 'Jinseok',
+        profilePicture: '',
+        date: '',
         photos: ['photo url'],
-        isAdmin: false,
+        content:
+            '전기정보공학부 소속의 과 동아리이지만, 전기과 학생들 말고도 타 공대나 자연대, 더 나아가 미대 학생들도 같이 참여하고 있습니다. 동아리의 앱 개발이 한창 진행중입니다.',
+        isAdmin: true,
     },
 ]
 
@@ -58,22 +71,28 @@ const renderItem = ({ item }: { item: Post }) => {
     return (
         <PostView>
             <ProfileView>
-                <Image source={profile} style={{ width: 42, height: 42 }} />
-                <Text style={{ fontSize: 14, padding: 8 }}>{item.author}</Text>
+                <ProfileImage
+                    source={{
+                        uri: 'https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=620&quality=85&auto=format&fit=max&s=21718fb1379918410ea10054db89f665',
+                    }}
+                />
+                <Text style={{ fontSize: 14, padding: 8 }}>
+                    {item.authorName}
+                </Text>
             </ProfileView>
-            {item.isAdmin && <AdminImage source={adminmark} />}
+            {item.isAdmin && <AdminImage source={adminMark} />}
             <ContentText numberOfLines={2}>{item.content}</ContentText>
-            <MoreText>더보기{'>'}</MoreText>
-            <View style={{ height: 131, flexDirection: 'row' }}>
+            <MoreText>더보기{' >'}</MoreText>
+            <ImageView>
                 <Image
                     source={picture}
-                    style={{ width: 138, height: 131, marginHorizontal: 5 }}
+                    style={{ width: 138, height: 132, margin: 5 }}
                 />
                 <Image
                     source={picture}
-                    style={{ width: 138, height: 131, marginHorizontal: 5 }}
+                    style={{ width: 138, height: 132, margin: 5 }}
                 />
-            </View>
+            </ImageView>
         </PostView>
     )
 }
@@ -86,35 +105,45 @@ export default function Post() {
     return (
         <View
             style={{
-                flex: 1, // 의미는 모르겠지만 이렇게 해야 잘 된다
+                //flex: 1을 없애고 <Post/>를 감싸고 있던 <View>에서 style 속성을 없애니 괜찮아졌다.
+                // 의미는 모르겠지만 이렇게 해야 잘 된다
+                alignItems: 'center',
                 justifyContent: 'center',
             }}
         >
             <View
                 style={{
-                    borderWidth: 1,
                     width: 334,
                     height: 40,
+                    borderWidth: 1,
                     top: 45,
                     position: 'absolute',
+                    alignItems: 'center',
+                    justifyContent: 'center',
                 }}
-            />
+            >
+                <Text>Search</Text>
+            </View>
             <View
                 style={{
-                    borderWidth: 1,
                     width: 334,
                     height: 53,
+                    borderWidth: 1,
                     top: 95,
                     position: 'absolute',
+                    alignItems: 'center',
+                    justifyContent: 'center',
                 }}
-            />
+            >
+                <Text>공지 3개</Text>
+            </View>
             <FlatList
-                showsHorizontalScrollIndicator={false}
-                style={{ marginTop: 170 }} // 검색창과 공지를 위한 공간
-                data={DATA}
+                // showsHorizontalScrollIndicator={false}
+                style={{ marginTop: 160 }} // 검색창과 공지를 위한 공간
                 ListHeaderComponent={ListHeader}
+                data={DATA}
                 renderItem={renderItem}
-                keyExtractor={(item: Post) => String(item.id)}
+                keyExtractor={(item: Post) => item.id}
             />
         </View>
     )
--- src/MainScreen/PostStyles.ts
+++ src/MainScreen/PostStyles.ts
@@ -1,50 +1,71 @@
 import styled from '@emotion/native'
+import { Platform } from 'react-native'
 
 export const TitleText = styled.Text({
-    paddingLeft: 6,
-    paddingBottom: 7,
+    marginLeft: 30,
+    marginBottom: 7,
     fontSize: 14,
     fontWeight: 'bold',
 })
 
 export const PostView = styled.View({
-    width: 335,
-    height: 267,
+    //width: 335,
+    //height: 300,
+    marginHorizontal: 24,
     marginBottom: 16,
     paddingHorizontal: 20,
-    paddingBottom: 20,
     backgroundColor: '#FFFFFF',
-    borderRadius: 23,
+    borderRadius: 24,
     borderWidth: 1,
     borderColor: '#C4C4C4',
-    shadowColor: 'rgba(0,0,0,0.25)',
+    shadowColor: Platform.select({
+        android: '#000000',
+        default: 'rgba(0,0,0,0.25)',
+    }),
     shadowRadius: 9,
     shadowOffset: { width: 3, height: 4 },
+    elevation: 10,
+})
+
+export const ProfileView = styled.View({
+    height: 42,
+    marginTop: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+})
+
+export const ProfileImage = styled.Image({
+    width: 42,
+    height: 42,
+    borderRadius: 21,
+    borderWidth: 1,
+    borderColor: 'black',
 })
 
 export const AdminImage = styled.Image({
     position: 'absolute',
-    top: 3,
+    top: 5,
     left: 48,
     width: 21,
     height: 21,
 })
 
-export const ProfileView = styled.View({
-    height: 42,
-    marginTop: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
-})
-
 export const ContentText = styled.Text({
-    height: 38,
     marginTop: 10,
     fontSize: 12,
+    lineHeight: 19,
 })
 
 export const MoreText = styled.Text({
-    height: 19,
+    marginTop: 5,
+    lineHeight: 19,
     fontSize: 12,
     textAlign: 'right',
+})
+
+export const ImageView = styled.View({
+    height: 156,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
 })
