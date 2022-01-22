7b241c1604aa8258c9b060d0e2210066fe71aef9
Jihoon Seo <jihoon416@snu.ac.kr>
2022-01-22 17:16:07 +0900

    feat: 스크롤에 따른 애니메이션 추가

--- src/PrinterReservation/view/PrinterDayHorizontalSelectionView.tsx
+++ src/PrinterReservation/view/PrinterDayHorizontalSelectionView.tsx
@@ -1,32 +1,76 @@
 import { useMemo } from 'react'
-import { Dimensions, Text, View } from 'react-native'
+import { Dimensions, View } from 'react-native'
 import Carousel from 'react-native-reanimated-carousel'
 import { CarouselRenderItem } from 'react-native-reanimated-carousel/lib/typescript/types'
 import { addDays, eachDayOfInterval, format } from 'date-fns'
+import Animated, {
+    interpolate,
+    interpolateColor,
+    useAnimatedStyle,
+} from 'react-native-reanimated'
 
 type Props = {
     selectedDate: Date
     onSelectedDateChange: (date: Date) => void
 }
 
-const renderItem: CarouselRenderItem<Date> = (itemInfo) => {
+const RenderItem: CarouselRenderItem<Date> = (itemInfo) => {
     const date = itemInfo.item
+    const { animationValue } = itemInfo
+
+    const viewAnimatedStyle = useAnimatedStyle(() => {
+        const backgroundColor = interpolateColor(
+            animationValue.value,
+            [-1, 0, 1],
+            ['white', 'blue', 'white'],
+        )
+
+        return {
+            backgroundColor,
+        }
+    }, [animationValue])
+
+    const textAnimatedStyle = useAnimatedStyle(() => {
+        const color = interpolateColor(
+            animationValue.value,
+            [-1, 0, 1],
+            ['black', 'white', 'black'],
+        )
+
+        const fontSize = interpolate(
+            animationValue.value,
+            [-1, 0, 1],
+            [12, 16, 12],
+        )
+
+        return {
+            color,
+            fontSize,
+        }
+    }, [animationValue])
+
     return (
-        <View
-            style={{
-                flex: 1,
-                marginVertical: 16,
-                marginHorizontal: 16,
-                backgroundColor: 'white',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                borderRadius: 8,
-            }}
+        <Animated.View
+            style={[
+                {
+                    flex: 1,
+                    marginVertical: 16,
+                    marginHorizontal: 16,
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderRadius: 8,
+                },
+                viewAnimatedStyle,
+            ]}
         >
-            <Text>{format(date, 'M/d')}</Text>
-            <Text>{format(date, 'eee')}</Text>
-        </View>
+            <Animated.Text style={textAnimatedStyle}>
+                {format(date, 'M/d')}
+            </Animated.Text>
+            <Animated.Text style={textAnimatedStyle}>
+                {format(date, 'eee')}
+            </Animated.Text>
+        </Animated.View>
     )
 }
 
@@ -44,12 +88,13 @@ export const PrinterDayHorizontalSelectionView = (props: Props) => {
         <View>
             <Carousel
                 data={data}
-                renderItem={renderItem}
+                renderItem={RenderItem}
                 style={{
                     width: Dimensions.get('window').width,
                     justifyContent: 'center',
                     alignItems: 'center',
                 }}
+                windowSize={5} // 퍼포먼스 증가 위한 prop - 한 번에 5개만 렌더
                 width={120}
                 loop={false}
             />
