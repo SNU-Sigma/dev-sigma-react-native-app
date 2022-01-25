8361e3c6ef1e5dca7ffa5623c64bb0a90a865226
Jihoon Seo <jihoon416@snu.ac.kr>
2022-01-25 21:29:11 +0900

    refactor: LoginAPI 재구성

--- src/service/LoginAPI.ts
+++ src/service/LoginAPI.ts
@@ -1,12 +1,27 @@
-export const LoginAPI = {
-    login: async (params: { email: string; password: string }) => {
+import { LoginInfo } from '../common/model/LoginInfo'
+
+export const LoginAPI = (() => {
+    let loginInfo: LoginInfo | undefined
+
+    const login = async (info: LoginInfo) => {
         // TODO: 추후 실제 API 연동 필요
-        if (
-            params.email === 'test_email' &&
-            params.password === 'test_password'
-        ) {
-            return Promise.resolve()
-        }
-        return Promise.reject('올바르지 않은 계정 정보입니다.')
-    },
-}
+        return new Promise<void>((resolve, reject) => {
+            setTimeout(() => {
+                if (info.email === 'sigma' && info.password === '1234') {
+                    resolve()
+                    loginInfo = info
+                }
+                reject('올바르지 않은 계정 정보입니다.')
+            }, 1000)
+        })
+    }
+
+    const getLoginInfo = () => {
+        return loginInfo
+    }
+
+    return {
+        login,
+        getLoginInfo,
+    }
+})()
