64890052636c2b540dcdf93f97ee3debaf3d809a
Jaewoo <tlsbamtol@snu.ac.kr>
2022-01-26 16:55:53 +0900

    Splash Screen added but not operating

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
-import * as React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Entypo } from '@expo/vector-icons'
+import * as SplashScreen from 'expo-splash-screen'
+import * as Font from 'expo-font'
 import LogIn from './src/LogInScreen/LogIn'
 import Main from './src/MainScreen/Main'
+import SplashScreenSIGMA from './src/SplashScreen/SplashScreenSIGMA'
 
 type Props = { navigation: any }
 
@@ -12,23 +17,53 @@ function LogInScreen({ navigation }: Props) {
 
 const Stack = createNativeStackNavigator()
 
-function App() {
+export default function App() {
+    const [appIsReady, setAppIsReady] = useState(false)
+    console.log('function', appIsReady)
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+                console.log('useEffect', appIsReady)
+                await SplashScreen.preventAutoHideAsync()
+                await Font.loadAsync(Entypo.font)
+                await new Promise((resolve) => setTimeout(resolve, 2000))
+            } catch (e) {
+                console.warn(e)
+            } finally {
+                setAppIsReady(true)
+            }
+        }
+        prepare()
+    }, [])
+
+    const onLayoutRootView = useCallback(async () => {
+        console.log('onLayoutRootView', appIsReady)
+        if (appIsReady) {
+            await SplashScreen.hideAsync()
+        }
+    }, [appIsReady])
+
+    if (!appIsReady) {
+        return <SplashScreenSIGMA />
+    }
+
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name='LogIn'
-                    component={LogInScreen}
-                    options={{ title: 'Overview', headerShown: false }}
-                />
-                <Stack.Screen
-                    name='Main'
-                    component={Main}
-                    options={{ headerShown: false }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <View onLayout={onLayoutRootView}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name='LogIn'
+                        component={LogInScreen}
+                        options={{ title: 'Overview', headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name='Main'
+                        component={Main}
+                        options={{ headerShown: false }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </View>
     )
 }
-
-export default App
---
+++ src/SplashScreen/SplashScreenStyles.ts
@@ -0,0 +1,21 @@
+import styled from '@emotion/native'
+
+export const Mark = styled.Image({
+    position: 'absolute',
+    width: 86,
+    height: 86,
+    top: 308,
+})
+
+export const Title = styled.Text({
+    position: 'absolute',
+    height: 68,
+    top: 394,
+
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 36,
+    lineHeight: 68,
+
+    color: '#000000',
+})
